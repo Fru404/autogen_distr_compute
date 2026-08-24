e6b9563c13bd0b57c5e37ecbe216c15b56ae259e
Fru Ngwa <dohng28@gmail.com>
2026-08-24 05:16:17 +0100

    Fix: app name undefined

--- main.py
+++ main.py
@@ -79,23 +79,7 @@ WORKER_URLS = [
     if url.strip()
 ]
 
-app.add_middleware(
-    CORSMiddleware,
 
-    allow_origins=[
-        "*"
-    ],
-
-    allow_credentials=True,
-
-    allow_methods=[
-        "*"
-    ],
-
-    allow_headers=[
-        "*"
-    ],
-)
 # ------------------------------------------------------------
 # Security
 # ------------------------------------------------------------
@@ -168,6 +152,23 @@ app = FastAPI(
     version="2.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=[
+        "*"
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=[
+        "*"
+    ],
+
+    allow_headers=[
+        "*"
+    ],
+)
 
 # ============================================================
 # IN-MEMORY JOB STORE
